6c0e0aabb5648739cb3c57e4055bb1501c12fe56
Gargiuloalano006 <gargiuloalano@gmail.com>
2026-03-08 22:24:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,21 +7,34 @@ CHAT_ID = os.environ.get("CHAT_ID")
 
 BOT_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-feeds = [
-    "https://www.motorsport.com/rss/f1/news/",
-    "https://www.fia.com/news/rss",
-    "https://www.formulapassion.it/feed",
-    "https://www.formula1.com/en/latest/all.xml"
-]
+feeds = {
+    "FIA": {
+        "url": "https://www.fia.com/news/rss",
+        "icon": "🚨‼️"
+    },
+    "FormulaPassion": {
+        "url": "https://www.formulapassion.it/feed",
+        "icon": "🟡"
+    },
+    "Motorsport": {
+        "url": "https://www.motorsport.com/rss/f1/news/",
+        "icon": "🔵"
+    },
+    "F1.com": {
+        "url": "https://www.formula1.com/en/latest/all.xml",
+        "icon": "🟢"
+    }
+}
 
-for feed_url in feeds:
-    feed = feedparser.parse(feed_url)
+for source, info in feeds.items():
+    feed = feedparser.parse(info["url"])
 
     for entry in feed.entries[:2]:
         title = entry.title
         link = entry.link
+        icon = info["icon"]
 
-        message = f"🏁 F1 NEWS\n\n{title}\n{link}"
+        message = f"{icon} {source}\n\n{title}\n{link}"
 
         data = {
             "chat_id": CHAT_ID,
