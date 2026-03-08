36c1ce2331fec39599cf88e418586b77b63fc486
Gargiuloalano006 <gargiuloalano@gmail.com>
2026-03-08 22:17:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,25 @@
 import requests
 import os
+import feedparser
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+BOT_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-message = "Bot avviato correttamente ✅"
+RSS_URL = "https://feeds.bbci.co.uk/news/technology/rss.xml"
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": message
-}
+feed = feedparser.parse(RSS_URL)
 
-requests.post(url, data=data)
+for entry in feed.entries[:3]:
+    title = entry.title
+    link = entry.link
+
+    message = f"📰 {title}\n{link}"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(BOT_URL, data=data)
