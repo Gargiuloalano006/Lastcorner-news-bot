856969cb65850e669cf9b66b19a5a824408ae62c
Gargiuloalano006 <gargiuloalano@gmail.com>
2026-03-08 22:16:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import requests
 import os
 
-TOKEN = os.environ['8433209125:AAHLqmdjUxvKX8W_WysgzxdtD5P5z9WLQgs']
-CHAT_ID = os.environ['-1003723380516']
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 message = "Bot avviato correttamente ✅"
 
 data = {
-    "chat_id": -1003723380516,
+    "chat_id": CHAT_ID,
     "text": message
 }
 
